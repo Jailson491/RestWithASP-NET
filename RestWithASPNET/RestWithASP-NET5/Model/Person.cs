355e765895e1b16e4d6df428387d9be2b8e683ff
Jailson Ferreira Silva <jailson491@hotmail.com>
2021-03-11 10:28:37 -0300

    Commit Classe Person

---
+++ RestWithASPNET/RestWithASP-NET5/Model/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASP_NET5.Model
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LstName { get; set; }
+        public string Adress { get; set; }
+        public string Gender { get; set; }
+
+    }
+}
